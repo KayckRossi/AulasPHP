b679c592d32662950a2cc38d39ad00b24ae943ef
KayckRossi <Kayck.12gabriel@gmail.com>
2022-08-24 22:16:17 -0300

    FODASE 2408

---
+++ contaCorrente.php
@@ -0,0 +1,11 @@
+<?php 
+    class contaCorrente extends Conta {
+        var $limite;
+
+        //Método construtor (sobrescrito) agora, também inicializa a variável $limite
+        function __construct($agencia, $codigo, $dataDeCriacao, $titular, $senha, $saldo, );
+        $this->limite= $limite;
+    }
+    {
+        
+    }
